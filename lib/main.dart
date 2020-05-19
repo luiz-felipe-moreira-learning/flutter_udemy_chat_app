8a6004ebeadab34f9d35eda18fb8ff5c35988d55
Luiz Felipe Moreira <felipecbm@gmail.com>
2020-05-19 16:05:11 -0300

    create project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_udemy_chat_app/screens/chat_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
